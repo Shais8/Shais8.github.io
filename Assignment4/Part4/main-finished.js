eb586d44f341cb8f6862f631dae9c5d180102634
Shais8 <sameer.shaikh1@dcmail.ca>
2024-03-21 19:12:38 -0400

    Added the evil ball
    Added the evil ball class with its constructor and methods.

--- Assignment4/Part4/main-finished.js
+++ Assignment4/Part4/main-finished.js
@@ -25,6 +25,57 @@ class Shape{
     this.velY = velY;
   }
 }
+class EvilCircle extends Shape{
+  constructor(x, y){
+    super(x, y, 20, 20);
+    this.color = 'white'
+    this.size = 10;
+  window.addEventListener("keydown", (e) => {
+      switch (e.key) {
+        case "a":
+          this.x -= this.velX;
+          break;
+        case "d":
+          this.x += this.velX;
+          break;
+        case "w":
+          this.y -= this.velY;
+          break;
+        case "s":
+          this.y += this.velY;
+          break;
+      }
+    });
+  }
+  draw() {
+    ctx.beginPath();
+    ctx.lineWidth = 3;
+    ctx.fillStyle = this.color;
+    ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
+    ctx.stroke();
+  }
+  update() {
+    if (this.x + this.size >= width) {
+      this.velX = -Math.abs(this.velX);
+    }
+
+    if (this.x - this.size <= 0) {
+      this.velX = Math.abs(this.velX);
+    }
+
+    if (this.y + this.size >= height) {
+      this.velY = -Math.abs(this.velY);
+    }
+
+    if (this.y - this.size <= 0) {
+      this.velY = Math.abs(this.velY);
+    }
+
+    this.x += this.velX;
+    this.y += this.velY;
+  }
+  
+}
 class Ball extends Shape{
   constructor(x, y, velX, velY, color, size, exits) {
     super(x, y, velX, velY);
