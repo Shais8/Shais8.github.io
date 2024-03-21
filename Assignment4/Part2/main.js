b2428f1f7ddf83323e6fdf986bb450f8577fed13
Shais8 <sameer.shaikh1@dcmail.ca>
2024-03-21 16:31:20 -0400

    Updated the loop
    Added the loop function, and the event listener for the images to switch when clicked.

--- Assignment4/Part2/main.js
+++ Assignment4/Part2/main.js
@@ -5,16 +5,23 @@ const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
 /* Declaring the array of image filenames */
-const images = ['pc1.jpg', 'pc2.jpg', 'pc3.jpg', 'pc4.jpg', 'pc5.jpg'];
+const images = ['pic1.jpg', 'pic2.jpg', 'pic3.jpg', 'pic4.jpg', 'pic5.jpg'];
 
 /* Declaring the alternative text for each image file */
-const altText = {'pc1.jpg': 'human eye', 'pc2.jpg': 'brown waves', 'pc3.jpg': 'purple flowers', 'pc4.jpg': 'egyption pharoah wall', 'pc5.jpg': 'Butterfly'}
+const altText = {'pic1.jpg' : 'human eye', 'pic2.jpg' : 'brown waves', 'pic3.jpg' : 'purple flowers', 'pic4.jpg' : 'egyption pharoah wall', 'pic5.jpg' : 'Butterfly'}
 
 /* Looping through images */
-
+for(let i=0; i<images.length; i++){
 const newImage = document.createElement('img');
-newImage.setAttribute('src', xxx);
-newImage.setAttribute('alt', xxx);
+const imageName = images[i];
+const imagePath = 'images/' + imageName;
+newImage.setAttribute('src', imagePath);
+newImage.setAttribute('alt', altText[imageName]);
 thumbBar.appendChild(newImage);
+newImage.addEventListener('click', event=> 
+{displayedImage.src = event.target.src;
+displayedImage.alt = event.target.alt;
+});
 
+}
 /* Wiring up the Darken/Lighten button */
