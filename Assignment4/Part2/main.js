3985f41416e69609f594a4af8909f9e2e3731626
Shais8 <sameer.shaikh1@dcmail.ca>
2024-03-21 16:02:48 -0400

    Added Arrays
    Added the array and the list, for images and alt text.

--- Assignment4/Part2/main.js
+++ Assignment4/Part2/main.js
@@ -5,8 +5,10 @@ const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
 /* Declaring the array of image filenames */
+const images = ['pc1.jpg', 'pc2.jpg', 'pc3.jpg', 'pc4.jpg', 'pc5.jpg'];
 
 /* Declaring the alternative text for each image file */
+const altText = {'pc1.jpg': 'human eye', 'pc2.jpg': 'brown waves', 'pc3.jpg': 'purple flowers', 'pc4.jpg': 'egyption pharoah wall', 'pc5.jpg': 'Butterfly'}
 
 /* Looping through images */
 
